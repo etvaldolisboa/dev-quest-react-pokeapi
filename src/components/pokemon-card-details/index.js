8209039c0ef2189c4654f7cf404299e3e54e13b0
Etvaldo Lisboa <etvaldolisboa@hotmail.com>
2023-11-26 15:16:39 -0400

    layout do component PokemonDetails alterado

--- src/components/pokemon-card-details/index.js
+++ src/components/pokemon-card-details/index.js
@@ -18,52 +18,90 @@ export const PokemonDetails = ({
   const PokeList = styled.main`
     padding: var(--p-mobile);
     display: flex;
+    min-height: 100vh;
     align-items: center;
     justify-content: center;
     flex-direction: column;
     background-color: ${theme.bgColor} ;
-  `
+    `
   const CardDetails = styled.div`
     margin: 10vh 0 2vh;
-    max-width: 76.8rem;
+    max-width: 124rem;
+    display: grid;
+    grid-template-areas:
+     'title title' 
+     'imagaDetails description';
+    grid-template-columns: 35% auto;
     border-radius: 1.8rem;
-    padding: 2vh 2vw;
-    border: 1px solid;
+    padding: 2rem;
     color: ${theme.color};
     background-color: ${theme.bgPrimary};
+    gap: 15px;
   
     h2{
-      font-size: var(--fs-big);
+      grid-area: title;
+      margin: 1.5rem 0;
+      font-size: var(--fs-extra);
+      font-weight: var(--fw-bold);
       text-align: center; 
     }
-  
   `
   const ImageDetails = styled.div`
-    border-radius: .6rem;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border: 1px solid;
-    margin: 1.5vh 0;
-    background-color: ${theme.bgInfo};
-    
+    grid-area: imagaDetails;
+    position: relative;
+
+    .image{
+      position: relative;
+      background: ${theme.bgInfo};
+      border-radius: 1.8rem;
+    }
+
     img{
       padding: 1rem;
-      max-width: 22.6rem;
-      width: 50%;
-      filter:
-      brightness(90%) contrast(85%) saturate(80%);
+      max-width: 48rem;
+    }
+    .id{
+      position: absolute;
+      color: ${theme.color};
+      bottom: 0;
+      left: 1rem;
+    }
+  
+    div{
+      display: flex;
+      justify-content: space-around;
+      margin-top: 1rem;
+      align-items: center;
+    }
+
+    h3{
+      margin: 1rem 0;
+      font-size: var(--fs-big);
+    }
+
+    ul, li{
+      display: flex;
+      gap: 1rem;
+    }
+
+    span{
+      text-transform: capitalize;
+      font-size: var(--fs-normal);
+      background-color: ${theme.bgInfo};
+      padding: .5rem;
+      border-radius: 8px;
+      display: inline-flex;
     }
   `
   const Dates = styled.div`
+    grid-area: description;
     font-size: var(--fs-normal);
     text-transform: capitalize;
     text-align: justify;
 
     span, ul{
       background-color: ${theme.bgInfo};
-      padding: .2rem;
+      padding: .5rem;
       font-weight: var(--fw-light);
       border-radius: 8px;
       display: flex;
@@ -78,14 +116,13 @@ export const PokemonDetails = ({
       <CardDetails key={index}>
         <h2>info pokemon</h2>
         <ImageDetails>
-          <img src={image} alt={name} />
-        </ImageDetails>
-        <Dates>
-          <div>
-            <h3>name:</h3>
-            <span>{name} #{id}</span>
+          <div className="image">
+            <img src={image} alt={name} />
+            <h2 className="id">#{id}</h2>
           </div>
+
           <div>
+            <h3>name:  <span>{name}</span> </h3>
             <h3>types:</h3>
             <ul>
               {types?.map((type, index) => {
@@ -99,6 +136,8 @@ export const PokemonDetails = ({
               })}
             </ul>
           </div>
+        </ImageDetails>
+        <Dates>
           <div>
             <h3>abilities:</h3>
             {abilities.map((ability, index) => {
@@ -132,7 +171,7 @@ export const PokemonDetails = ({
                     </span>
                   </li>
                 )
-              }).slice(0, 15)}
+              }).slice(0, 30)}
             </ul>
           </div>
         </Dates>
